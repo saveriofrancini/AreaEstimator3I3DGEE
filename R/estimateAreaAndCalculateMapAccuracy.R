79684099c34a00de7928709bced69dd7670a8896
saveriofrancini <saverio.francini@gmail.com>
2021-04-12 11:32:48 +0200

    ref

--- R/estimateAreaAndCalculateMapAccuracy.R
+++ R/estimateAreaAndCalculateMapAccuracy.R
@@ -1,6 +1,7 @@
 #' estimateAreaAndCalculateMapAccuracy function
 #'
 #' Calculate area estimates and map accuracy using 3I3D-GEE outputs
+#'
 #' @return  list
 #' @keywords mapAccuracy
 #' @export
@@ -10,63 +11,62 @@
 # doi paper
 # ............................................
 
-estimateAreaAndCalculateMapAccuracy <- function(
-  counts = system.file("counts", "counts.csv", package = "AreaEstimator3I3DGEE"),
-  sample = system.file("sample", "sample.shp", package = "AreaEstimator3I3DGEE"),
-  outPath = getwd()
-){
+estimateAreaAndCalculateMapAccuracy <-
+  function(counts = system.file("counts", "counts.csv", package = "AreaEstimator3I3DGEE"),
+           sample = system.file("sample", "sample.shp", package = "AreaEstimator3I3DGEE"),
+           outPath = getwd()) {
+    # core code
+    # read inputs
+    counts <- read.table(counts, sep = ",", header = T)[, 2:4]
+    sample  <- raster::shapefile(sample)@data
+    # calculate area estimates
+    counts$nBuffer <-
+      gsub("[^0-9.-]", "", as.character(counts$nBuffer))
+    counts$nChange <-
+      gsub("[^0-9.-]", "", as.character(counts$nChange))
+    counts$nForest <-
+      gsub("[^0-9.-]", "", as.character(counts$nForest))
+    counts  <-
+      as.double(c(counts$nChange, counts$nBuffer, counts$nForest))
+    w <- counts / sum(counts)
+    cm <- table(sample$classesMap, sample$reference)
+    sum <- apply(cm, 1, sum)
+    ReferenceClasses  <- colnames(cm)
+    nReferenceClasses <- ncol(cm)
+    out <- NULL
+    for (ReferenceClasses_i in ReferenceClasses) {
+      ReferenceClasses_i_counts <-
+        as.double(cm[, colnames(cm) == ReferenceClasses_i])
+      p <- ReferenceClasses_i_counts / sum
+      pxw <- p * w
+      sumpxw <- sum(pxw)
+      Varp <- (p * (1 - p)) / sum
+      w2xVarp <- (w ^ 2) * Varp
+      sumw2xVarp <- sum(w2xVarp)
+      sqrtsumw2xVarp <- sqrt(sumw2xVarp)
+      area <- sumpxw
+      SE <-   sqrtsumw2xVarp
+      SEpercentage <- SE / area * 100
+      CI <- SE * 2
+      out <-
+        rbind(out, c(ReferenceClasses_i, area, SE, SEpercentage, CI))
+    }
+    colnames(out) <-
+      c("referenceClass", "area", "SE", "SE%", "CI")
+    areaEstimates <- as.data.frame(out)
+    write.table(areaEstimates, file.path(outPath, "areaEstimates.csv"), row.names = F)
 
-# core code
-# read inputs
-counts <- read.table(counts, sep = ",", header = T)[,2:4]
-sample  <- raster::shapefile(sample)@data
-# calculate area estimates
-counts$nBuffer <- gsub("[^0-9.-]", "", as.character(counts$nBuffer))
-counts$nChange <- gsub("[^0-9.-]", "", as.character(counts$nChange))
-counts$nForest <- gsub("[^0-9.-]", "", as.character(counts$nForest))
-counts  <- as.double(c(counts$nChange, counts$nBuffer, counts$nForest))
-areaTot <- sum(counts)/100 # ha
-w <- counts/sum(counts)
-cm <- table(sample$classesMap, sample$reference)
-sum <- apply(cm, 1, sum)
-ReferenceClasses  <- colnames(cm)
-nReferenceClasses <- ncol(cm)
-out <- NULL
-for (ReferenceClasses_i in ReferenceClasses) {
-ReferenceClasses_i_counts <- as.double(cm[,colnames(cm)==ReferenceClasses_i])
-p <- ReferenceClasses_i_counts/sum
-pxw <- p*w
-sumpxw <- sum(pxw)
-Varp <- (p*(1-p))/sum
-w2xVarp <- (w^2)*Varp
-sumw2xVarp <- sum(w2xVarp)
-sqrtsumw2xVarp <- sqrt(sumw2xVarp)
-area <- sumpxw*areaTot
-SE <-   sqrtsumw2xVarp*areaTot
-SEpercentage <- SE/area*100
-CI <- SE*2
-out <- rbind(out, c(ReferenceClasses_i, area, SE, SEpercentage, CI))
-}
-colnames(out) <- c("referenceClass", "area(ha)", "SE(ha)", "SE%", "CI(ha)")
-areaEstimates <- out
-write.table(out, file.path(outPath, "areaEstimates.csv"), row.names = F)
+    # ............................................
+    # calculate map accuracy
+    commission <- (cm[1, 2] / sum(cm[1, 1:2])) * 100
+    omission <- (cm[3, 1] / sum(cm[3, 1:2])) * 100
+    disturbedBuffer <- (cm[2, 1] / sum(cm[2, 1:2])) * 100
 
-# ............................................
-# calculate map accuracy
-
-TP <- cm[1,1]
-FP <- cm[1,2]
-FN <- cm[3,1]
-TN <- cm[3,2]
-bufferDisturbance <- cm[2,1]
-bufferUndisturbedForest <- cm[2,2]
-omissions               <- round(FN/(TP+FN)*100, 1)
-commissions             <- round(FP/(TP+FP)*100, 1)
-percentageOfBufferBeingDisturbance <- round(bufferDisturbance/(bufferDisturbance+bufferUndisturbedForest)*100, 1)
-percentageOfBufferBeingNoDisturbance <- round(bufferUndisturbedForest/(bufferDisturbance+bufferUndisturbedForest)*100, 1)
-out <- cbind(omissions, commissions, percentageOfBufferBeingDisturbance, percentageOfBufferBeingNoDisturbance)
-mapAccuracy <- out
-#colnames(out) <- c("referenceClass", "area", "SE", "SE%", "CI")
-write.table(out, file.path(outPath, "mapAccuracy.csv"), row.names = F)
-return(list(areaEstimates, mapAccuracy))
-}
+    out <-
+      cbind(round(commission, 2),
+            round(omission, 2),
+            round(disturbedBuffer, 2))
+    colnames(out) <- c("commission (%)", "omission (%)", "bufferPredictedAsDisturbed (%)")
+    write.table(out, file.path(outPath, "mapAccuracy.csv"), row.names = F)
+    return(list(areaEstimates, out))
+  }
--- README.md
+++ README.md
@@ -1,28 +1,31 @@
-<<<<<<< HEAD
 ![banner image](logo/graphical_abstract.bmp) 
-=======
-![banner image](logo/graphicalAbstract.bmp) 
->>>>>>> 6b7599074afb6cdd38bd0df6bd1c0cac3f6a2529
 
 # AreaEstimator3I3DGEE
 
-**An R package to calculate area estimates using 3I3D-GEE outputs**
+**An R package to estimate forest disturbance areas using 3I3D-GEE outputs**
 
 <br><br>
 
-## [Manuscript](https://www.topolino.it/character/pluto/) 
-
-## Citation
-
-<<<<<<< HEAD
-> Saverio Francini*, Ronald E. McRoberts, Giovanni D’Amico, Nicholas C. Coops, Txomin Hermosilla, Joanne C. White, Michael A. Wulder, Marco Marchetti, Giuseppe Scarascia Mugnozza and Gherardo Chirici
-=======
-> Francini et al.
->>>>>>> 6b7599074afb6cdd38bd0df6bd1c0cac3f6a2529
+## [Manuscript](https://doi.org/10.1016/j.jag.2021.102663) 
 
 <br><br>
 
 ## [Google Earth Engine user interface](https://code.earthengine.google.com/?accept_repo=users/sfrancini/S23I3D) 
+This R package calculates forest disturbance areas estimates using [3I3D-GEE](https://code.earthengine.google.com/?accept_repo=users/sfrancini/S23I3D) outputs.
+The [3I3D-GEE](https://code.earthengine.google.com/?accept_repo=users/sfrancini/S23I3D) link creates an "S23I3D" repository that you can find into the "Reader" section of the "Scripts" window on your Google Earth Engine account.
+The S23I3D repository includes two JavaScript codes: (i) "library" is the code that includes all the functions, and (ii) "run" is the code that allows executing the application and generating the user interface.
+To execute the appliction via the user friendly interface, open the "run" code and click on the "Run" button. 
+
+A detailed documentation of the application is in the [appendix](https://www.sciencedirect.com/science/article/pii/S0303243421003706?via%3Dihub#f0015) of the manuscript. 
+
+## AreaEstimator3I3DGEE documentation
+
+[Forest disturbance areas](https://www.sciencedirect.com/science/article/pii/S0303243421003706?via%3Dihub#s0055) and [map accuracies](https://www.sciencedirect.com/science/article/pii/S0303243421003706?via%3Dihub#s0060) can be estimated using the 3I3D-GEE outputs and this R package. AreaEstimator3I3DGEE uses the counts table and the photointerpreted sample as inputs and outputs two CSV files: (i) areaEstimates.csv with the area, SE, and confidence interval estimates for each class defined in the photointerpretation phase (e.g. forest fire, clear-cut and wind damage), and (ii) mapAccuracy.csv with map accuracy assessment in terms of [omission](https://www.sciencedirect.com/science/article/pii/S0303243421003706?via%3Dihub#e0010) and [commission](https://www.sciencedirect.com/science/article/pii/S0303243421003706?via%3Dihub#e0015) errors.
+
+The `AreaEstimator3I3DGEE` R package includes just one function `estimateAreaAndCalculateMapAccuracy()` which requires three parameters: 
+- 1 `counts`: the path for the "counts.csv" file you downloaded using 3I3D-GEE.
+- 2 `sample`: the path for the "sample.shp" file you downloaded using 3I3D-GEE. The sample is selected using [this](https://www.sciencedirect.com/science/article/pii/S0303243421003706?via%3Dihub#s0045) procedure and needs to be photointerpreted as detailed in the section 3.5. of the [manuscript](https://doi.org/10.1016/j.jag.2021.102663).
+- 3 `outPath`: the path to save outputs
 
 ## Install and Test the package
 
@@ -32,17 +35,11 @@ To do it, paste the code below in the R Console and press enter
 `install.packages("devtools", repos = "http://cran.us.r-project.org")`
 `devtools::install_github("saveriofrancini/AreaEstimator3I3DGEE")`
 
-#### Execute the package using test data distributed within the package.
+#### Execute the package using test (dummy) data distributed within the package.
 To do it, paste the code below in the R Console and press enter.
 
 `AreaEstimator3I3DGEE::estimateAreaAndCalculateMapAccuracy()`
 
-If everything went fine, outputs are now in your working directory. 
-Of course you can specify a different folder for outputs and use the package with your own 3I3D-GEE outputs.
-
 <br><br>
-<<<<<<< HEAD
-=======
 
-###### Thanks for stopping by and have a great day!
->>>>>>> 6b7599074afb6cdd38bd0df6bd1c0cac3f6a2529
+> Saverio Francini, Ronald E. McRoberts, Giovanni D’Amico, Nicholas C. Coops, Txomin Hermosilla, Joanne C. White, Michael A. Wulder, Marco Marchetti, Giuseppe Scarascia Mugnozza and Gherardo Chirici. 2022. An open science and open data approach for the statistically robust estimation of forest disturbance areas. International Journal of Applied Earth Observation and Geoinformation. https://doi.org/10.1016/j.jag.2021.102663
